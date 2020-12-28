318cc20a280eaff6b593dfafc80141685408028a
PedroASA <m.amaral.hhs@gmail.com>
2020-12-28 16:39:14 -0300

    :point_right: Add JQuery and Bug Fix

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import TicTacToe from './dist/tic-tac-toe.js';
 
 function my_reset() {
     alert("Reset Button!");
-    document.querySelectorAll('.my-button').forEach(
-        t => {
-    
-            t.style.background = "none";
-            t.disabled= false;
-            t.onmouseenter = function () { t.style.backgroundColor = "#eaeaea";};
-            t.onmouseleave = function () { t.style.background = "none";};
-        });
-
-        tic_tac_toe = new TicTacToe();
+    $(' .my-button').each(
+        function() {
+            this.style.background = "none";
+            this.disabled= false;
+            $( this ).on("mouseenter", function () { this.style.backgroundColor = "#eaeaea";});
+            $( this ).on("mouseleave",function () { this.style.background = "none"; });
+            //$( this ).off(onclick);
+        }
+    )
+        
+    tic_tac_toe = new TicTacToe();
 }
 
 function my_hint () {
@@ -24,55 +25,56 @@ function addImage (button, image, size) {
     button.style.backgroundRepeat = "no-repeat";
     button.style.backgroundPosition = "center";
     button.disabled = true;
+
+
     tic_tac_toe.put(parseInt(button.id, 10));
+
+    if(!tic_tac_toe.is_over()) {
+        let t = tic_tac_toe.util(tic_tac_toe.winner()) === 1 ? "X" : "O";
+        alert(`Player ${t} has won the game!`);
+        rmvBtnList();
+    }
 }
  
-function putX(button) {
-    addImage(button, "./images/X.png", "50% 80%");
+function putX() {
+    addImage(this, "./images/X.png", "50% 80%");
+    addBtnList(putO);
+
 }
 
-function putO (button) {
-    addImage(button, "./images/O.png", "70% 90%");
+function putO () {
+    addImage(this, "./images/O.png", "70% 90%");
+    addBtnList(putX);
 }
 
-const my_func = (func, x) => () => func(x);
-
+function inv(f) {
+    if(f === "putO") 
+        putX;
+    else
+        putO;
+}
 
 function addBtnList (func) {
-    document.querySelectorAll('.my-button').forEach(
-        it => {
-            if(!it.disabled)
-                it.addEventListener("click", my_func(func, it));
+    $('.my-button').each(
+        function () {
+            $( this ).off("click", inv(`${func}`));
+            if(!this.disabled)
+                $( this ).on("click", func);
         }
         
    )
 }
 
-function rmvBtnList(func) {
-    document.querySelectorAll('.my-button').forEach(
-        it => {
-            if(!it.disabled)
-                it.removeEventListener("click", my_func(func, it));
+
+function rmvBtnList() {
+    $('.my-button').each(
+        function () {
+            if(!this.disabled) {
+                $( this ).off();
+                this.disabled = true;
+            }
         }      
-   )
-}
-
-//MAIN LOOP
-
-function loop () {
-    if(!tic_tac_toe.is_over()) {
-        if(tic_tac_toe.player() === 0) {
-            addBtnList(putX);
-        }
-        else {
-            addBtnList(putO);
-        }
-    }
-    else {
-        let t = tic_tac_toe.util(tic_tac_toe.winner()) === 1 ? "X" : "O";
-        alert(`Player ${t} has won the game!`);
-        rmvBtnList(t === "X" ? putX: putO);
-    }
+    )
 }
 
 //INIT
@@ -80,17 +82,18 @@ function loop () {
 
 var tic_tac_toe = new TicTacToe();
 
+
 document.addEventListener('DOMContentLoaded', 
 () => {
     document.getElementById('rst-btn').onclick = my_reset;
     document.getElementById('hnt-btn').onclick = my_hint;
         let i = 0;
-        document.querySelectorAll('.my-button').forEach(
-            it => {
-                it.addEventListener("click", loop);
-                it.id = i++;
+        $('.my-button').each(
+            function () {
+                $( this ).id = i++;
             }
         );
-        loop();
+
+        addBtnList(putX);
     }
 );
